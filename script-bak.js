be05baa590940e6feab64ad55d60bea75cbf6848
Burchell <jlkaline@me.com>
2020-12-29 18:18:19 -0500

    updated js and added styling

--- script-bak.js
+++ script-bak.js
@@ -1,4 +1,9 @@
 //declare variables
+var questionOne = document.getElementById("questionOne");
+var questionTwo = document.getElementById("questionTwo");
+var questionThree = document.getElementById("questionThree");
+var questionFour = document.getElementById("questionFour");
+var questionFive = document.getElementById("questionFive");
 var score = 0;
 var timeEl = document.querySelector(".time");
 var quizBtn = document.querySelector("#quiz");
@@ -6,56 +11,43 @@ var quizBtn = document.querySelector("#quiz");
 var allQuestions = [
     {
         question: "What does HTML stand for?" , 
-        choices: ["Hometext Markup Language", "Hypertext Markup Language", "Hyperlink Text Markup Language"],
-        answer: 1,
-        answerGiven:-1
+        choices: ["Hometext Markup Language", "Hypetext Markup Language", "Hyperlink Text Markup Language"],
+        answer: "Hometext Markup Language"
     },
     {
         question: "The bootstrap grid system is based on how many columns?" , 
         choices: ["9", "6", "12"],
-        answer: 2,
-        answerGiven:-1
+        answer: 2
     },
     {
         question: "What does CSS stand for?" , 
         choices: ["Cascading Style Sheets", "Creative Style Sheets", "Computer Style Sheets"],
-        answer: 0,
-        answerGiven:-1
+        answer: "Cascading Style Sheets"
     },
     {
         question: "How do you create a function in JavaScript" , 
-        choices: ["function = myFunction()", "function: myFunction()", "function myFunction"],
-        answer: 1,
-        answerGiven:-1
+        choices: ["function = myFunction()", "function: myFunction()", "function myFunction()"],
+        answer: "function myFunction()"
     },
     {
         question: "What is the correct way to write an array in JavaScript" , 
         choices: ["var num = '1','2','3';", "var num = ['1','2','3']", "var num =('1','2','3')"],
-        answer: 1,
-        answerGiven:-1
-    }
+        answer: "var num = ['1','2','3']"
+    },
     
-];
-var highScores = [
-    {initials: "BT", score: 99} 
 ];
 
 var mainEl = document.getElementById("main");
-var timer = document.getElementById("timer");
+var timer;
 var bodyEl = document.createElement("div");
 
 var i = 0;
 var timeLeft = 75;
 
-var currentQuestion=0;
+
 
 function startQuiz() {
-  mainEl.append(bodyEl);
-//  alert("start quiz");
-
-  $("#section").hide();
-  $("#divScoreContainer").hide();
-  $("#divQuizContent").show();
+ // mainEl.append(bodyEl);
   showQuestion()
 
   var quizInterval = setInterval(function() {
@@ -64,7 +56,7 @@ function startQuiz() {
       endgame(); 
     } else {
       timeLeft--;
-      timer.innerHTML = "Time: " + timeLeft;
+      timer.textContent = "Time: " + timeLeft;
     }
 
   }, 1000);
@@ -76,7 +68,7 @@ function endgame() {
 
 }
 
-//startQuiz();
+startQuiz();
 
 quizBtn.addEventListener("click", startQuiz);
 
@@ -93,61 +85,26 @@ quizBtn.addEventListener("click", startQuiz);
 //}
 
 function showQuestion() {
-    //alert("show question");
-    const question = allQuestions[currentQuestion];
-    var questionText = "<p>"+question.question+"</p>";
-    for (var i in question.choices)
-    {
-        questionText+= "<button onClick='answer("+i+")'>"+question.choices[i]+"</button><br/>";
+    $("button").click(function() {
+        $("div").empty();
+
+    document.getElementById("startButton");
+    if (i === allQuestions.length) {
+        getscore();
     }
-    //alert(questionText);
-    $("#divQuizContent").html(questionText);
-};
-
-function answer(idx)
-{
-    if (idx==allQuestions[currentQuestion].answer)
-    {
-        alert("correct");
-    }else{
-        timeLeft -= 10;
-        alert("wrong: "+timeLeft);
-    }
-    allQuestions[currentQuestion].answerGiven = idx;
-    currentQuestion++;
-    if (currentQuestion===allQuestions.length)
-    {
-        endQuiz();
-    }else{
-        showQuestion();
-    }
-}
-function endQuiz()
-{
-    $("#divQuizContent").hide();
-    $("#divScoreContainer").html ("Score is: "+timeLeft);
-    $("#section").hide();
-    alert("Score is: "+timeLeft);
-}
-function clearHighScore()
-{
+   
+});
 
 }
-function goBack ()
-{
-
-}
-
-/*
 function loadQuestion (questionIndex) {
 	var q = questions[questionIndex];
     questionEl.textContent = (questionIndex + 1) + '. ' + q.question;
     
     choiceBtn.addEventListener("click", chooseAnswer);
-
+}
 function chooseAnswer() {
     var selectedChoices = document.querySelector('click');
-        
+
         var answer = selectedChoices.value;
         if(questions[allQuestion].answer === answer){
            //correct 
@@ -176,9 +133,8 @@ function chooseAnswer() {
     //if answer is correct more to next question
     //if incorrect deduct 10 sec 
     //after choose answers and out of questions move to high score page
-}
+
 
 function getScore() {
 
 }
-*/
