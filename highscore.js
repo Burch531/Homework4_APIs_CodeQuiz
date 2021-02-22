f821d00f8611ca437dc3ee2f4623b9c164bdcdc7
Burchell <jlkaline@me.com>
2021-02-21 19:57:04 -0500

    fixed highscores

--- highscore.js
+++ highscore.js
@@ -1,18 +1,18 @@
-var ulScore= document.querySelector(".listScore");
+var ulScore = document.querySelector(".listScore");
 
 
 function clearHighScore() {
     localStorage.removeItem("allScores");
-  }
+}
 
 // Local Storage
 var allScores = localStorage.getItem("allScores");
-allScores= JSON.parse(allScores);
+allScores = JSON.parse(allScores);
 
 if (allScores !== null) {
-    for (var i = 0; i <allScores.length; i++) {
+    for (var i = 0; i < allScores.length; i++) {
         var scoreLi = document.createElement("li");
-        scoreLi.setAttribute("id","highScore initials");
+        scoreLi.setAttribute("highScore", "initials");
         scoreLi.textContent = allScores[i].initials + " " + allScores[i].highScore;
         ulScore.appendChild(scoreLi);
     }
